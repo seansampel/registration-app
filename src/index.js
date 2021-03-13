b07cb0716aeefa250c1012b2635122be343de79c
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-03-13 11:54:29 +0000

    add hcomponents and router file wwith basic markup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
+
+ReactDom.render(<AppRouter />, document.getElementById('root'));
